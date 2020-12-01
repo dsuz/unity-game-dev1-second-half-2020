0cdcdf53e2011d4a5ae620a8c488593b6efd216f
D. Suzuki <bboydaisuke@gmail.com>
2020-12-02 00:33:26 +0900

    １週目の内容とパッケージを追加した

---
+++ Assets/Scripts/MarkerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// クリックで Ray を飛ばして、Ray を Scene ウィンドウに表示するコンポーネント
+/// 適当な GameObject に追加して使う
+/// </summary>
+public class MarkerController : MonoBehaviour
+{
+    /// <summary>Ray が何にも当たらなかった時、Scene に表示する Ray の長さ</summary>
+    [SerializeField] float m_debugRayLength = 100f;
+    /// <summary>Ray が何かに当たった時に Scene に表示する Ray の色</summary>
+    [SerializeField] Color m_debugRayColorOnHit = Color.red;
+    /// <summary>ここに GameObject を設定すると、飛ばした Ray が何かに当たった時にそこに m_marker オブジェクトを移動する</summary>
+    [SerializeField] GameObject m_marker;
+    /// <summary>飛ばした Ray が当たった座標に m_marker を移動する際、Ray が当たった座標からどれくらいずらした場所に移動するかを設定する</summary>
+    [SerializeField] Vector3 m_markerOffset = Vector3.up * 0.01f;
+
+    void Update()
+    {
+        // クリックで Ray を飛ばす
+        if(Input.GetButtonDown("Fire1"))
+        {
+            // カメラの位置 → マウスでクリックした場所に Ray を飛ばすように設定する
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit; // out パラメータで Ray の衝突情報を受け取るための変数
+            // Ray を飛ばして、コライダーに当たったかどうかを戻り値で受け取る
+            bool isHit = Physics.Raycast(ray, out hit); // オーバーライドがたくさんあることに注意すること
+
+            // Ray が当たったかどうかで異なる処理をする
+            if (isHit)
+            {
+                // Ray が当たった時は、当たった座標まで赤い線を引く
+                Debug.DrawLine(ray.origin, hit.point, m_debugRayColorOnHit);
+                // m_marker がアサインされていたら、それを移動する
+                if (m_marker)
+                {
+                    m_marker.transform.position = hit.point + m_markerOffset;
+                }
+            }
+            else
+            {
+                // Ray が当たらなかった場合は、Ray の方向に白い線を引く
+                Debug.DrawRay(ray.origin, ray.direction * m_debugRayLength);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerControllerAi.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.AI;   // Navmesh Agent を使うために必要
+
+/// <summary>
+/// Navmesh Agent を使って経路探索を行い、移動するためのコンポーネント
+/// </summary>
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerControllerAi : MonoBehaviour
+{
+    /// <summary>移動先となる位置情報</summary>
+    [SerializeField] Transform m_target;
+    /// <summary>移動先座標を保存する変数</summary>
+    Vector3 m_cachedTargetPosition;
+    /// <summary>キャラクターなどのアニメーションするオブジェクトを指定する</summary>
+    [SerializeField] Animator m_animator;
+    NavMeshAgent m_agent;
+
+    void Start()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+        m_cachedTargetPosition = m_target.position; // 初期位置を保存する（※）
+    }
+
+    /*
+     * （※）m_cachedTargetPosition を使って座標を保存しているのは、以下の Update() 内で「毎フレーム座標をセットする」という処理を避け、負荷を下げるためである。
+     * 毎フレーム座標をセットすることで経路の計算を毎フレームしてしまうことを避けるため、「Target が移動した時のみ」目的地をセットして経路の計算を行わせている。
+     */
+
+    void Update()
+    {
+        // m_target が移動したら Navmesh Agent を使って移動させる
+        if (Vector3.Distance(m_cachedTargetPosition, m_target.position) > 0.1f) // m_target が 10cm 以上移動したら
+        {
+            m_cachedTargetPosition = m_target.position; // 移動先の座標を保存する
+            m_agent.SetDestination(m_cachedTargetPosition); // Navmesh Agent に目的地をセットする（Vector3 で座標を設定していることに注意。Transform でも GameObject でもなく、Vector3 で目的地を指定する）
+        }
+
+        // m_animator がアサインされていたら Animator Controller にパラメーターを設定する
+        if (m_animator)
+        {
+            m_animator.SetFloat("Speed", m_agent.velocity.magnitude);
+        }
+    }
+}
